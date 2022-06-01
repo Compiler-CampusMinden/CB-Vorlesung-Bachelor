3f8851e1f278c09180e329d86b0f37f062d4e02a
Florian Warzecha <florian_filip.warzecha@fh-bielefeld.de>
2022-06-01 08:52:38 +0200

    tooling: reformat delete-script.rb

--- .github/actions/alpine-pandoc-hugo/delete-script.rb
+++ .github/actions/alpine-pandoc-hugo/delete-script.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 # delete-script.rb
 # Usage: ./delete-script.rb [dir]
@@ -19,52 +20,50 @@
 
 START_MARKER = '<!-- REM'
 END_MARKER = 'REM -->'
-FILE_EXT = ".md"
+FILE_EXT = '.md'
 
 class RemSyntaxError < StandardError
 end
 
 def remove_block(path, start_line, message, end_line, initial_path)
-    scope = path.delete_suffix(File.extname path)
-    if initial_path.end_with?("/")
-        scope = scope.delete_prefix(initial_path)
-    else
-        scope = scope.delete_prefix(initial_path + "/")
+  scope = path.delete_suffix(File.extname(path))
+  scope = if initial_path.end_with?('/')
+            scope.delete_prefix(initial_path)
+          else
+            scope.delete_prefix(initial_path + '/')
+          end
+
+  commit = "rm(#{scope}): #{message}"
+
+  puts "Deleting #{path}:#{start_line + 1} - #{path}:#{end_line + 1} with commit message '#{commit}'"
+
+  lines = IO.readlines(path)
+
+  File.open(path, mode: 'w') do |f|
+    lines.each_with_index do |line, index|
+      f.write(line) if index < start_line || index > end_line
     end
+  end
 
-    commit = "rm(#{scope}): #{message}"
-
-        puts "Deleting #{path}:#{start_line + 1} - #{path}:#{end_line + 1} with commit message '#{commit}'"
-
-        lines = IO.readlines(path)
-
-    File.open(path, mode: "w") do |f|
-        lines.each_with_index do |line, index|
-            if index < start_line || index > end_line
-                f.write(line)
-            end
-        end
-    end
-
-    system("git add '#{path}'", exception: true)
-    system("git commit -m '#{commit}'", exception: true)
+  system("git add '#{path}'", exception: true)
+  system("git commit -m '#{commit}'", exception: true)
 end
 
 def process_file_once(path, initial_path)
   start_line = false
-  message = ""
+  message = ''
   end_line = false
 
   IO.readlines(path).each_with_index do |line, index|
-
     if !start_line && line.start_with?(START_MARKER)
       start_line = index
-      message = line.delete_prefix(START_MARKER + "").strip()
+      message = line.delete_prefix(START_MARKER + '').strip
     elsif start_line
       if line.start_with?(END_MARKER)
         end_line = index
       elsif line.start_with?(START_MARKER)
-        raise RemSyntaxError, "Found nested '#{START_MARKER}' at #{path}:#{index + 1}! (outer block starts at L#{start_line + 1})"
+        raise RemSyntaxError, "Found nested '#{START_MARKER}' at #{path}:#{index + 1}!"\
+          "(outer block starts at L#{start_line + 1})"
       end
     elsif line.start_with?(END_MARKER)
       raise RemSyntaxError, "Found '#{END_MARKER}' without corresponding '#{START_MARKER}' at #{path}:#{index + 1}"
@@ -77,18 +76,16 @@ def process_file_once(path, initial_path)
     raise RemSyntaxError, "Missing '#{END_MARKER}' at #{path}:EOF! (block starts at L#{start_line + 1})"
   elsif start_line && end_line
     remove_block(path, start_line, message, end_line, initial_path)
-    return true
+    true
   else
-    return false
+    false
   end
 end
 
 def process_file_all(file, initial_path)
   # process file until there are no more blocks to be removed
   deleted = true
-  while deleted
-      deleted = process_file_once(file, initial_path)
-  end
+  deleted = process_file_once(file, initial_path) while deleted
 end
 
 def process_dir(path, initial_path)
@@ -97,26 +94,26 @@ def process_dir(path, initial_path)
   Dir.each_child(path) do |entry|
     entry_path = File.join(path, entry)
 
-    if Dir.exists?(entry_path)
+    if Dir.exist?(entry_path)
       errors += process_dir(entry_path, initial_path)
     elsif File.file?(entry_path) && File.extname(entry_path) == FILE_EXT
       begin
         process_file_all(entry_path, initial_path)
-      rescue => e
+      rescue StandardError => e
         errors << e
       end
     end
   end
 
-  return errors
+  errors
 end
 
 dir = if ARGV.length == 1
         ARGV[0]
       else
-        "."
+        '.'
       end
 errors = process_dir(dir, dir)
 errors.each do |error|
-  $stderr.puts "#{error.class}: #{error.message}"
+  warn "#{error.class}: #{error.message}"
 end
