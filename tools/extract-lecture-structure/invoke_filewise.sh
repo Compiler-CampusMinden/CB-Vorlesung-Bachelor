23bd995b5212a57cb4a5a8baf0430f8e32de98de
mr <malt.r@mail.de>
2022-06-01 09:52:43 +0200

    Remove structure-related filters and temporary invokation scripts

--- tools/extract-lecture-structure/invoke_filewise.sh
+++
@@ -1,2 +0,0 @@
-mkdir .tmp_structure
-make web_sources | xargs -n 1 bash -c 'pandoc "$0" --lua-filter=tools/extract-lecture-structure/filewise_pass.lua -o .tmp_structure/temp.md; cat .tmp_structure/temp.md >> .tmp_structure/temp_summary.md'
--- tools/extract-lecture-structure/invoke_summary.sh
+++
@@ -1,2 +0,0 @@
-pandoc .tmp_structure/temp_summary.md --lua-filter=tools/extract-lecture-structure/summary_pass.lua -o structure.md
-rm -rf .tmp_structure
