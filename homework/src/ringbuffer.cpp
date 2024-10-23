474c17d7203dcf6824b1e6daa063e2c129f1fcae
Carsten Gips <cagix@hsbi.de>
2024-10-23 18:49:17 +0200

    further implementation

--- homework/src/ringbuffer.cpp
+++ homework/src/ringbuffer.cpp
@@ -1,54 +1,46 @@
+#include <cstdlib>      // size_t
+//#include <cstddef>
+#include <iostream>
+
+using namespace std;
+
 
 class Token {
 public:
     /**
-     * Constructor
+     * Constructs a new token object.
      *
-     * Constructs a new smart pointer from a raw pointer, sets the reference
-     * counter to 1.
-     *
-     * @param p is a raw pointer to the object to be shared
+     * @param lexem is a pointer to the text of the token (to be copied)
+     * @param row is the row in input where this token was found
+     * @param col is the column in input where this token starts
      */
-    Token(Token * const p = nullptr);
+    Token(const char* lexem, int row, int col);
 
     /**
-     * Copy constructor
-     *
-     * Constructs a new smart pointer from another smart pointer, increments the
-     * reference counter.
-     *
-     * @param p is another smart pointer
-     */
-    Token(const Token& p);
-
-    /**
-     * Destructor
-     *
-     * Decrements the reference counter. If it reaches zero, the shared object will
-     * be free'd.
+     * Destructs the token object and free's the stored lexem.
      */
     ~Token();
 
     /**
-     * Dereferences the smart pointer
+     * Access to the token's lexem
      *
-     * @return a pointer to the shared object
+     * @return a pointer to the stored lexem
      */
-    char* lexem() const;
+    char* lexem();
 
     /**
-     * Dereferences the smart pointer
+     * Access to the token's row
      *
-     * @return a pointer to the shared object
+     * @return the row where this token was found
      */
-    int row() const;
+    int row();
 
     /**
-     * Dereferences the smart pointer
+     * Access to the token's column
      *
-     * @return a pointer to the shared object
+     * @return the column where this token starts
      */
-    int col() const;
+    int col();
 
 private:
     char* lexem;    ///< Pointer to the text of the token
@@ -65,15 +57,15 @@ public:
      * Constructs a new smart pointer from a raw pointer, sets the reference
      * counter to 1.
      *
-     * @param p is a raw pointer to the object to be shared
+     * @param p is a raw pointer to the token to be shared
      */
-    SmartToken(Token * const p = nullptr);
+    SmartToken(const Token* p = nullptr);
 
     /**
      * Copy constructor
      *
-     * Constructs a new smart pointer from another smart pointer, increments the
-     * reference counter.
+     * Constructs a new smart pointer from another smart pointer, increments
+     * the reference counter.
      *
      * @param p is another smart pointer
      */
@@ -82,61 +74,53 @@ public:
     /**
      * Destructor
      *
-     * Decrements the reference counter. If it reaches zero, the shared object will
-     * be free'd.
+     * Decrements the reference counter. If it reaches zero, the shared token
+     * will be free'd.
      */
     ~SmartToken();
 
     /**
      * Assignment
      *
-     * Changes the shared object, thus we need first to do the same
-     * like the destructor, then something like the constructor.
-     *
-     * @param p raw pointer to the new object
-     */
-    const SmartToken &operator=(Token * const p);
-
-    /**
-     * Assignment
-     *
-     * Changes the shared object, thus we need first to do the same
-     * like the destructor, then something like the copy constructor.
+     * Changes the shared token, thus we need first to do the same like the
+     * destructor, followed by something like the constructor.
      *
      * @param p is another smart pointer
      */
-    const SmartToken &operator=(const SmartToken& p);
+    SmartToken& operator=(const SmartToken& p);
 
     /**
      * Dereferences the smart pointer
      *
-     * @return a reference to the shared object
+     * @return a reference to the shared token
      */
-    Token &operator*() const;
+    Token& operator*();
 
     /**
      * Dereferences the smart pointer
      *
-     * @return a pointer to the shared object
+     * @return a pointer to the shared token
      */
-    Token *operator->() const;
+    Token* operator->();
 
     /**
      * Comparison
      *
-     * @return true, if `p` shares the same object
+     * @return true, if `p` shares the same token
      */
     bool operator==(const SmartToken& p) const;
 
 private:
-    Token* pObj;            ///< Pointer to the current shared object
-    RefCounter *rc;     ///< Pointer to the reference counter (used for the current object)
+    Token* pObj;        ///< Pointer to the current shared token
+    RefCounter* rc;     ///< Pointer to the reference counter (used for the current token)
+
+    releaseObject();                         ///< Decrement reference counter and free object if necessary
+    acquireObject(Token* p, RefCounter* r);  ///< Copy pointer to token and reference counter and increment reference counter
 };
 
 
 class RefCounter {
 public:
-
     /**
      * Default constructor
      */
@@ -159,30 +143,23 @@ public:
      */
     bool isZero() const;
 
-    /**
-     * Returns count
-     *
-     * @return n
-     */
-    unsigned int getRefCount() const;
-
     // Hide copy constructor and assignment operator
     RefCounter(const RefCounter&) = delete;
-    RefCounter &operator=(const RefCounter&) = delete;
+    RefCounter& operator=(const RefCounter&) = delete;
 
 private:
     unsigned int n;     ///< How many SmartToken share ownership of "our" object?
 };
 
 
-template <typename T, size_t size, typename alloc_t = std::allocator<T>>
 class RingBuffer {
 public:
     /**
      * Constructor that creates a new ring buffer for max. `size` elements
      *
-     * Initialises the attributes and allocates memory for `size` elements of type `T*` and let the
-     * pointer `elems` point to this new array
+     * Initialises the attributes and allocates memory for `size` elements
+     * of type `SmartToken` and let the pointer `elems` point to this new
+     * array
      *
      * @param size is the max. number of elements that can be stored
      */
@@ -191,42 +168,38 @@ public:
     /**
      * Destructor
      *
-     * 1. Frees all elements using an allocator of type `alloc_t`
-     * 2. Frees the dynamically allocated array `elems`
+     * free's the dynamically allocated array `elems`
      */
     ~RingBuffer();
 
     /**
      * Reading the first (oldest) element
      *
-     * If an element has been read, the `head` points to the next element and `count` is decremented.
-     * The read element is **not** released.
+     * If an element has been read, the `head` points to the next element
+     * and `count` is decremented. The read element is **not** released.
      *
-     * @return Returns a pointer to the first (i.e. oldest) element of the buffer, but does not (yet)
-     * delete it; returns `nullptr` if buffer is empty
+     * @return Returns (a copy of) the first (i.e. oldest) element of the
+     * buffer, but does not (yet) release it; returns an empty `SmartToken`
+     * if buffer is empty
      */
-    T* readBuffer();
+    SmartToken readBuffer();
 
     /**
      * Adding a new element
      *
-     * Appends the new element to the end of the queue. If the buffer is full, the oldest element will be
-     * deleted with an allocator of type `alloc_t` and the pointer to the new element will be inserted in
-     * this location.
+     * Appends the new element to the end of the queue. If the buffer is
+     * full, the oldest element will be overwritten by the new element. The
+     * old element will take care of releasing its memory (smart pointer).
      *
-     * @param data is a pointer to the element to be added (allocated with an allocator of type `alloc_t`)
+     * @param data is a reference to the element to be added
      */
-    void writeBuffer(T *data);
-
-    /**
-     * Indicates the number of elements in the ring buffer relative to its size on the LED display
-     */
-    void displayStatus() const;
+    void writeBuffer(const SmartToken& data);
 
 private:
     size_t count;           ///< number of elements currently stored in the buffer
     size_t head;            ///< points to the beginning of the buffer (oldest element)
-    T **elems;              ///< array with `size` places of type `T*`, dynamically allocated
+    size_t size;            ///< length of array `elems`
+    SmartToken* elems;      ///< array with `size` places of type `SmartToken`, dynamically allocated
 };
 
 
@@ -242,131 +215,59 @@ private:
 
 
 
-/*
- * SmartPointerImpl.h
- *
- */
-
-#ifndef SMARTPOINTERIMPL_H
-#define SMARTPOINTERIMPL_H
-
-#include <cstddef>
-#include <cassert>
-
-#include "smartpointer/NullPointerException.h"
-#include "smartpointer/SmartToken.h"
-
 
 /*
  * ===========================================================================
- * Eigentliche Smartpointer-Klasse (Implementierung: Template -> .h)
- *
  *                                   MUSTERLOESUNG
  * ===========================================================================
  */
 
-template<typename T>
-SmartToken<T>::SmartToken(T * const ptr) :
-        pObj(ptr), rc((ptr != nullptr) ? new RefCounter() : nullptr) {
-    if (rc) {
-        rc->inc();
+Token::Token(const char* l, int r, int c) : row(r), col(c) {
+    lexem = new char[strlen(l)];
+    strcpy(lexem, l);
+}
+Token::~Token() { delete lexem; }
+char* Token::lexem() { return lexem; }
+int Token::row() { return row; }
+int Token::col() { return col; }
+
+
+SmartToken::SmartToken(const Token* ptr) {
+    acquireObject(ptr, (ptr != nullptr) ? new RefCounter() : nullptr);
+}
+SmartToken::SmartToken(const SmartToken& sp) {
+    acquireObject(sp.pObj, sp.rc);
+}
+SmartToken::~SmartToken() {
+    releaseObject();
+}
+SmartToken& SmartToken::operator=(const SmartToken& p) {
+    if (pObj == sp.pObj) {
+        return *this;
     }
-}
 
-template<typename T>
-SmartToken<T>::SmartToken(const SmartToken<T> &sp) :
-        pObj(sp.pObj), rc(sp.rc) {
-    if (rc) {
-        rc->inc();
-    }
-}
+    releaseObject();
+    acquireObject(p.pObj, p.rc)
 
-template<typename T>
-SmartToken<T>::~SmartToken() {
-    deleteObject();
+    return *this;
 }
-
-template<typename T>
-T *SmartToken<T>::operator->() const {
-    if (pObj == nullptr) {
-        throw NullPointerException();
-    }
-    return pObj;
-}
-
-template<typename T>
-T &SmartToken<T>::operator*() const {
+Token& SmartToken::operator*() {
     if (pObj == nullptr) {
         throw NullPointerException();
     }
     return *pObj;
 }
-
-template<class T>
-const T *SmartToken<T>::getObject() const {
+Token* SmartToken::operator->() {
+    if (pObj == nullptr) {
+        throw NullPointerException();
+    }
     return pObj;
 }
-
-template<class T>
-const RefCounter *SmartToken<T>::getRefCounter() const {
-    return rc;
-}
-
-template<typename T>
-const SmartToken<T> &SmartToken<T>::operator=(T * const p) {
-    if (p == pObj) {
-        return *this;
-    }
-
-    deleteObject();
-
-    if (p != nullptr) {
-        rc = new RefCounter();
-        rc->inc();
-    } else {
-        rc = nullptr;
-    }
-    pObj = p;
-
-    return *this;
-}
-
-template<typename T>
-const SmartToken<T> &SmartToken<T>::operator=(const SmartToken<T> &sp) {
-    if (pObj == sp.pObj) {
-        return *this;
-    }
-
-    deleteObject();
-
-    rc = sp.rc;
-    pObj = sp.pObj;
-    if (rc != nullptr) {
-        rc->inc();
-    }
-
-    return *this;
-}
-
-template<typename T>
-bool SmartToken<T>::operator==(const SmartToken<T> &sp) const {
+bool SmartToken::operator==(const SmartToken& sp) const {
     return pObj == sp.pObj;
 }
-
-template<typename T>
-bool SmartToken<T>::operator!=(const SmartToken<T> &sp) const {
-    return !(*this == sp);
-}
-
-template<class T>
-SmartToken<T>::operator bool() const {
-    return bool(pObj);
-}
-
-template<class T>
-void SmartToken<T>::deleteObject() {
+void SmartToken::releaseObject() {
     if (pObj) {
-        assert(rc != nullptr);
         rc->dec();
         if (rc->isZero()) {
             delete rc;
@@ -376,183 +277,48 @@ void SmartToken<T>::deleteObject() {
         }
     }
 }
-
-
-#endif  // SMARTPOINTERIMPL_H
-
-/*
- * RefCounter.cpp
- *
- */
-
-#include "smartpointer/RefCounter.h"
-
-/*
- * ===========================================================================
- * Hilfsklasse zum Zaehlen der Referenzen: Implementierung
- *
- *
- *                                  MUSTERLOESUNG
- * ===========================================================================
- */
-
-RefCounter::RefCounter() :
-        n(0) {
-}
-void RefCounter::inc() {
-    n++;
-}
-void RefCounter::dec() {
-    n--;
-}
-bool RefCounter::isZero() const {
-    return n == 0;
-}
-unsigned int RefCounter::getRefCount() const {
-    return n;
+void SmartToken::acquireObject(Token* p, RefCounter* r) {
+    pObj = p;
+    rc = r;
+    if (rc) rc->inc();
 }
 
 
-/*
- * RingBuffer.h
- *
- */
-
-#ifndef RINGBUFFER_H
-#define RINGBUFFER_H
+RefCounter::RefCounter() : n(0) {}
+void RefCounter::inc() { ++n; }
+void RefCounter::dec() { --n; }
+bool RefCounter::isZero() const { return n == 0; }
 
 
-#include <cstdlib>      // size_t
-#include <cstddef>
-#include <iostream>
+RingBuffer::RingBuffer(size_t size) : count(0), head(0), size(size) {
+    elems = new SmartToken[size];
+}
+RingBuffer::~RingBuffer() {
+    delete [] elems;
+}
+SmartToken RingBuffer::readBuffer() {
+    if (count) {
+        SmartToken& elem = elems[head];
+        head = (head + 1) % size;
+        --count;
+        return elem;
+    }
+    return SmartToken();
+}
+void RingBuffer::writeBuffer(const SmartToken& data) {
+    size_t tail;
 
-using namespace std;
-
-
-/**
- * Template class to represent a ring buffer
- *
- * @param T is the type of elements to be stored (via pointer of type `T*`)
- * @param size is the max. number of elements that can be stored
- * @param alloc_t is the type of the allocator used to allocate the pointers to the elements (optional)
- */
-template <typename T, size_t size, typename alloc_t = std::allocator<T>>
-class RingBuffer {
-public:
-    /**
-     * Constructor that creates a new ring buffer for max. `size` elements
-     *
-     * Initialises the attributes and allocates memory for `size` elements of type `T*` and let the
-     * pointer `elems` point to this new array
-     */
-    RingBuffer() : m_allocator(alloc_t()), count(0), head(0) {
-        elems = static_cast<T**>(malloc(size * sizeof(T*)));
-        if (!elems) throw bad_alloc();
+    /* Puffer voll? Head freigeben */
+    if (count == size) {
+        head = (head + 1) % size;
+        --count;
     }
 
-    /**
-     * Destructor
-     *
-     * 1. Frees all elements using an allocator of type `alloc_t`
-     * 2. Frees the dynamically allocated array `elems`
-     */
-    ~RingBuffer() {
-        /* Elemente mit m_allocator freigeben */
-        while (count) {
-            T* data = readBuffer();
-            m_allocator.deallocate(data, 1);
-        }
-
-        /* eigentlichen Puffer freigeben */
-        free(elems);
-    }
-
-    /**
-     * Reading the first (oldest) element
-     *
-     * If an element has been read, the `head` points to the next element and `count` is decremented.
-     * The read element is **not** released.
-     *
-     * @return Returns a pointer to the first (i.e. oldest) element of the buffer, but does not (yet)
-     * delete it; returns `nullptr` if buffer is empty
-     */
-    T* readBuffer() {
-        if (count) {
-            T *elem = elems[head];
-            head = (head + 1) % size;
-            --count;
-            return elem;
-        }
-        return nullptr;
-    }
-
-    /**
-     * Adding a new element
-     *
-     * Appends the new element to the end of the queue. If the buffer is full, the oldest element will be
-     * deleted with an allocator of type `alloc_t` and the pointer to the new element will be inserted in
-     * this location.
-     *
-     * @param data is a pointer to the element to be added (allocated with an allocator of type `alloc_t`)
-     */
-    void writeBuffer(T *data) {
-        size_t tail;
-
-        if (data) {
-            /* Puffer voll? Head freigeben */
-            if (count == size) {
-                m_allocator.deallocate(elems[head], 1);
-                head = (head + 1) % size;
-                --count;
-            }
-
-            /* Daten ins Ende schreiben */
-            tail = (head + count) % size;
-            elems[tail] = data;
-            ++count;
-        }
-    }
-
-    /**
-     * Indicates the number of elements in the ring buffer relative to its size on the LED display
-     */
-    void displayStatus() const {
-        cout << "RingBuffer: Anzahl der Elemente: " << count << endl;
-        cout << "RingBuffer: Kapazizät: " << size << " (noch " << size-count << " Elemente frei)" << endl;
-    }
-
-private:
-    alloc_t m_allocator;    ///< allocator to free element pointers
-
-    size_t count;           ///< number of elements currently stored in the buffer
-    size_t head;            ///< points to the beginning of the buffer (oldest element)
-    T **elems;              ///< array with `size` places of type `T*`, dynamically allocated
-};
-
-
-#endif  // RINGBUFFER_H
-
-
-
-
-
-
-
-
-/*
- * mainsmartpointer.cpp
- *
- *                                  MUSTERLOESUNG
- *
- *
- */
-
-#include <cstdlib>
-#include <iostream>
-
-#include "smartpointer/SmartToken.h"
-
-using namespace std;
+    /* Daten ins Ende schreiben */
+    tail = (head + count) % size;
+    elems[tail] = data;
+    ++count;
+}
 
 
 class Foo {
@@ -691,5 +457,5 @@ int main(int argc, char **argv) {
         cout << "x: " << *x << "; x-y: " << (long) x - long (y) << endl;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
