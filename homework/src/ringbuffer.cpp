4995a7fee41ccf45b3e30ad1ede37a1675e9d19c
Carsten Gips <cagix@hsbi.de>
2024-10-24 17:13:58 +0200

    add some printing

--- homework/src/ringbuffer.cpp
+++ homework/src/ringbuffer.cpp
@@ -27,6 +27,8 @@ public:
     char* lexem;    ///< Pointer to the text of the token
     int row;        ///< Row in input where this token was found
     int col;        ///< Column in input where this token starts
+
+friend ostream &operator<<(ostream &out, const Token &t);
 };
 
 
@@ -59,6 +61,8 @@ public:
     RefCounter& operator=(const RefCounter&) = delete;
 
     unsigned int n;     ///< How many SmartToken share ownership of "our" object?
+
+friend ostream &operator<<(ostream &out, const RefCounter &rc);
 };
 
 
@@ -129,6 +133,8 @@ private:
 
     void releaseObject();                           ///< Decrement reference counter and free object if necessary
     void acquireObject(Token* p, RefCounter* r);    ///< Copy pointer to token and reference counter and increment reference counter
+
+friend ostream &operator<<(ostream &out, const SmartToken &st);
 };
 
 
@@ -202,16 +208,19 @@ private:
  * ===========================================================================
  */
 
-Token::Token(const char* l, int r, int c) : row(r), col(c) {
-    cout << "Token::Token(): {lexem: " << l << ", row: " << r << ", column: " << c << "}" << endl;
-
+Token::Token(const char* l, int r, int c) : lexem(nullptr), row(r), col(c) {
     lexem = new char[strlen(l)+1];  // +1 for terminating '\0'
     strcpy(lexem, l);
+
+    cout << "Token::Token():" << *this << endl;
 }
 Token::~Token() {
-    cout << "Token::~Token(): {lexem: " << lexem << ", row: " << row << ", column: " << col << "}" << endl;
+    cout << "Token::~Token(): " << *this << endl;
 
     delete lexem;
+}
+ostream &operator<<(ostream &out, const Token &t) {
+    return out << "{lexem: " << t.lexem << ", row: " << t.row << ", column: " << t.col << "}";
 }
 
 
@@ -219,20 +228,26 @@ RefCounter::RefCounter() : n(0) { cout << "RefCounter::RefCounter()" << endl; }
 void RefCounter::inc() { ++n; }
 void RefCounter::dec() { --n; }
 bool RefCounter::isZero() const { return n == 0; }
+ostream &operator<<(ostream &out, const RefCounter &rc) {
+    return out << "{rc: " << rc.n << "}";
+}
 
 
 SmartToken::SmartToken(Token* ptr) : pObj(nullptr), rc(nullptr) {
-    cout << "SmartToken::SmartToken(Token*): {ptr: " << ptr << "}" << endl;
-
     if (ptr) acquireObject(ptr, new RefCounter());
+
+    cout << "SmartToken::SmartToken(Token*): " << *this << endl;
 }
-SmartToken::SmartToken(const SmartToken& sp) {
-    cout << "SmartToken::SmartToken(SmartToken&): {sp.pObj: " << sp.pObj << ", sp.rc: " << sp.rc << "}" << endl;
+SmartToken::SmartToken(const SmartToken& sp) : pObj(nullptr), rc(nullptr) {
+    cout << "SmartToken::SmartToken(SmartToken&): sp " << sp << endl;
+    cout << "SmartToken::SmartToken(SmartToken&): before " << *this << endl;
 
     acquireObject(sp.pObj, sp.rc);
+
+    cout << "SmartToken::SmartToken(SmartToken&): after " << *this << endl;
 }
 SmartToken::~SmartToken() {
-    cout << "SmartToken::~SmartToken(): {pObj: " << pObj << ", rc: " << rc << "}" << endl;
+    cout << "SmartToken::~SmartToken(): " << *this << endl;
 
     releaseObject();
 }
@@ -266,7 +281,7 @@ bool SmartToken::operator==(const SmartToken& sp) const {
     return pObj == sp.pObj;
 }
 void SmartToken::releaseObject() {
-    cout << "SmartToken::releaseObject(): {pObj: " << pObj << ", rc: " << rc << "}" << endl;
+    cout << "SmartToken::releaseObject(): " << *this << endl;
 
     if (pObj) {
         rc->dec();
@@ -280,17 +295,23 @@ void SmartToken::releaseObject() {
 }
 void SmartToken::acquireObject(Token* p, RefCounter* r) {
     cout << "SmartToken::acquireObject(): {p: " << p << ", r: " << r << "}" << endl;
-    cout << "SmartToken::acquireObject(): before {pObj: " << pObj << ", rc: " << rc << "}" << endl;
+    cout << "SmartToken::acquireObject(): before " << *this << endl;
 
     pObj = p;
     rc = r;
     if (rc) rc->inc();
 
-    cout << "SmartToken::acquireObject(): after {pObj: " << pObj << ", rc: " << rc << "}" << endl;
+    cout << "SmartToken::acquireObject(): after " << *this << endl;
+}
+ostream &operator<<(ostream &out, const SmartToken &st) {
+    if (st.pObj && st.rc)
+        return out << "{pObj: " << st.pObj << ", rc: " << st.rc << " >> " << *(st.pObj) << " >> " << *(st.rc);
+    else
+        return out << "{pObj: " << st.pObj << ", rc: " << st.rc << "}";
 }
 
 
-RingBuffer::RingBuffer(size_t size) : count(0), head(0), size(size) {
+RingBuffer::RingBuffer(size_t size) : elems(nullptr), count(0), head(0), size(size) {
     cout << "RingBuffer::RingBuffer(): {size: " << size << "}" << endl;
 
     elems = new SmartToken[size];
@@ -340,6 +361,8 @@ int main(int argc, char **argv) {
 
         cout << endl;
         delete foo;
+
+        cout << endl;
     }
 
 
@@ -349,10 +372,32 @@ int main(int argc, char **argv) {
 
         SmartToken st;                                                  // uses default c'tor (all empty)
         SmartToken wuppie = SmartToken(new Token("wuppie", 1, 4));      // default c'tor w/ pointer
-        SmartToken fluppie = SmartToken(wuppie);                        // copy c'tor
+        {
+            cout << endl;
+            SmartToken fluppie = SmartToken(wuppie);                    // copy c'tor
+            SmartToken fluppie2(wuppie);                                // copy c'tor
+            SmartToken foo = SmartToken(new Token("foo", 9, 35));       // default c'tor w/ pointer
+
+            cout << endl;
+            cout << "st == wuppie: " << (st == wuppie) << endl;             // 0 (no)
+            cout << "fluppie == wuppie: " << (fluppie == wuppie) << endl;   // 1 (yes)
+            cout << "foo == wuppie: " << (foo == wuppie) << endl;           // 0 (no)
+
+            cout << endl;
+            cout << "st: " << st << endl;               // {pObj: 0x0, rc: 0x0}
+            cout << "wuppie: " << wuppie << endl;       // {pObj: 0xN, rc: 0xM, lexem: wuppie, row: 1, col: 4, rc: 3}
+            cout << "fluppie: " << fluppie << endl;     // {pObj: 0xN, rc: 0xM, lexem: wuppie, row: 1, col: 4, rc: 3}
+            cout << "foo: " << foo << endl;             // {pObj: 0xO, rc: 0xP, lexem: foo, row: 9, col: 35, rc: 1}
+
+            cout << endl;
+        }
 
         cout << endl;
+        cout << "st: " << st << endl;               // {pObj: 0x0, rc: 0x0}
+        cout << "wuppie: " << wuppie << endl;       // {pObj: 0xN, rc: 0xM, lexem: wuppie, row: 1, col: 4, rc: 1}
 
+
+        cout << endl;
     }
 
 
