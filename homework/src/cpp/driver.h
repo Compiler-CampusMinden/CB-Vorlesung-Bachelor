4aa8155d2aa1b88f74a957a6e3b4c9b93b6e9abc
Carsten Gips <cagix@hsbi.de>
2025-01-11 14:07:56 +0100

    simplify setup

---
+++ homework/src/cpp/driver.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#define print_int(expr)  std::cout << "print_int("  << #expr << "): " << expr << std::endl;
+#define print_char(expr) std::cout << "print_char(" << #expr << "): " << expr << std::endl;
+#define print_bool(expr) std::cout << "print_bool(" << #expr << "): " << expr << std::endl;
