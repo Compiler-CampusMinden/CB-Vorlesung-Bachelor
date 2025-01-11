ffa5157d8f20372d01de1571fe9116a3af8c361c
Carsten Gips <cagix@hsbi.de>
2025-01-11 14:31:42 +0100

    add while

---
+++ homework/src/cpp/while.cpp
@@ -0,0 +1,25 @@
+
+int main() {
+    // simple while-loop
+    bool a = true;
+    while (a) {
+        print_bool(a);  // 1
+        a = false;
+    }
+
+    // more complex loop
+    int i = 0;
+    int arr[5];
+    while (i<5) {
+        arr[i] = i + 1;
+        i = i + 1;
+    }
+    print_int(arr[0]);  // 1
+    print_int(arr[1]);  // 2
+    print_int(arr[2]);  // 3
+    print_int(arr[3]);  // 4
+    print_int(arr[4]);  // 5
+
+
+    return 0;
+}
